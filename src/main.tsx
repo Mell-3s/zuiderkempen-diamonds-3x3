c989a49606757a713d567acc53d318b39c5758fa
Dries Mellaerts <dries.mellaerts.9@gmail.com>
2023-05-25 12:18:09 +0200

    MUI

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import { ThemeProvider } from '@emotion/react';
+import { createTheme, CssBaseline } from '@mui/material';
+
 import App from './App.tsx';
+
+import './index.css';
+
+const theme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#E3000B',
+    },
+  },
+  typography: {
+    fontFamily: 'Open Sans',
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
