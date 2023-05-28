22d981c9d8e6c3739be97ae69cb55aaa011a991c
Dries Mellaerts <dries.mellaerts.9@gmail.com>
2023-05-28 17:05:52 +0200

    vite-plugin-sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import Sitemap from 'vite-plugin-sitemap';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), Sitemap()],
 });
