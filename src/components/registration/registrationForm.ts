a76e5b4c8480b4f01c4cdd8f8b46618855ecad00
Dries Mellaerts <dries.mellaerts.9@gmail.com>
2023-06-04 22:50:13 +0200

    email send

--- src/components/registration/registrationForm.ts
+++ src/components/registration/registrationForm.ts
@@ -3,29 +3,24 @@ import * as z from "zod";
 
 function printPlayers(players: TPlayers): string {
   let message = "";
-  players.forEach((player, i) => (message += `${i}) ${player.name}, ${player.birthday} `));
+  players.forEach((player, i) => (message += `${i + 1}) ${player.name}, ${player.birthday} `));
   return message;
 }
 
 export function sendMail(form: TRegistrationForm) {
-  emailjs
-    .send(
-      "service_it2al0t",
-      "template_dp1wurc",
-      {
-        name: form.name,
-        category: form.category,
-        "captain.name": form.captain.name,
-        "captain.email": form.captain.email,
-        "captain.birthday": form.captain.birthday,
-        players: printPlayers(form.players),
-      },
-      "ZodAGtnrV4h9QOjTT",
-    )
-    .then(() => window.alert("Uw inschrijving is verzonden!"))
-    .catch(() =>
-      window.alert("Het is niet gelukt om uw inschrijving te versturen, probeer later opnieuw!"),
-    );
+  return emailjs.send(
+    "service_opovhpk",
+    "template_eiqhlnc",
+    {
+      name: form.name,
+      category: form.category,
+      captain_name: form.captain.name,
+      captain_email: form.captain.email,
+      captain_birthday: form.captain.birthday,
+      players: printPlayers(form.players),
+    },
+    "ZodAGtnrV4h9QOjTT",
+  );
 }
 
 const subCategoriesMale = ["U12 Jongens", "U14 Jongens", "U16 Jongens", "U18 + Seniors"] as const;
@@ -44,7 +39,7 @@ export const categoriesTabs = [
 const players = z
   .object({ name: z.string().min(3), birthday: z.string().min(3) })
   .array()
-  .min(4)
+  .min(3)
   .max(4);
 
 type TPlayers = z.infer<typeof players>;
---
+++ src/components/succeeded/Succeeded.tsx
@@ -0,0 +1,14 @@
+function Succeeded() {
+  return (
+    <>
+      <h2 className="text-2xl font-bold">Bedankt voor uw inschrijving!</h2>
+      <hr className="my-2 w-16 rounded border-y-4 border-red-500" />
+      <p>
+        Uw inschrijving is verzonden, u bent pas echt ingeschreven als wij een bevestings mail naar
+        u gestuurd hebben!
+      </p>
+    </>
+  );
+}
+
+export default Succeeded;
