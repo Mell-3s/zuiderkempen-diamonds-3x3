26e18d83cfd08cfd5c2a5501f04f191c39be60bf
Dries Mellaerts <dries.mellaerts.9@gmail.com>
2023-06-05 21:22:19 +0200

    fix typo space

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -6,7 +6,7 @@ function Footer() {
         <hr className="my-2 w-16 rounded border-y-4 border-red-500" />
         <p className="text-white">
           Het tornooi gaat door in het Sportpark Joris Verhaegen te Hulshout (Industriepark 3, 2235
-          Hulshout). Naast veel sportplezier zal er ook een hamburgerkraam aanwezig zijn alsook een
+          Hulshout). Naast veel sportplezier zal er ook een hamburgerkraam aanwezig zijn als ook een
           springkasteel. De winnaar van het tornooi ontvangt een mooie prijs!
         </p>
         <hr className="my-4" />
