c8d97ed5cb2bc34379f54998c195b45129b7f16e
Dries Mellaerts <dries.mellaerts.9@gmail.com>
2023-06-18 13:38:28 +0200

    U14 Mixed

--- src/components/registration/registrationForm.ts
+++ src/components/registration/registrationForm.ts
@@ -25,13 +25,13 @@ export function sendMail(form: TRegistrationForm) {
 
 const subCategoriesMale = [
   "U12 Mixed (2012 of jonger)",
-  "U14 Jongens (2010 of jonger)",
+  "U14 Mixed (2010 of jonger)",
   "U17 Jongens (2007 of jonger)",
   "U18 + Seniors",
 ] as const;
 const subCategoriesFemale = [
   "U12 Mixed (2012 of jonger)",
-  "U14 Meisjes (2010 of jonger)",
+  "U14 Mixed (2010 of jonger)",
   "U17 Meisjes (2007 of jonger)",
   "U18 + Dames",
 ] as const;
